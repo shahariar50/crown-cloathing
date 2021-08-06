347d61fd55c08110adf4dd5ea5f5647972c341ce
Shahariar Touhid <shahariar.touhid@gmail.com>
2021-08-06 12:13:39 +0600

    added homepage and menu item

---
+++ src/pages/homePage.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h3>Hats</h3>
+            <span>Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h3>Jackets</h3>
+            <span>Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h3>Snickers</h3>
+            <span>Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h3>Womens</h3>
+            <span>Shop Now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h3>Mens</h3>
+            <span>Shop Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
