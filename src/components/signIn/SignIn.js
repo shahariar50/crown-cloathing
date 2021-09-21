678de4d142a78625ae57b8e40af1e7de4355b099
Shahariar Touhid <shahariar.touhid@gmail.com>
2021-09-22 01:28:22 +0600

    finished firebase auth

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -1,5 +1,6 @@
+import { signInWithEmailAndPassword } from "@firebase/auth";
 import React from "react";
-import { signInWithGoogle } from "../../utils/firebase/firebase";
+import { auth, signInWithGoogle } from "../../utils/firebase/firebase";
 import Button from "../button/Button";
 import FormInput from "../formInput/FormInput";
 import "./signIn.styles.scss";
@@ -8,7 +9,23 @@ const SignIn = () => {
   const [loginData, setLoginData] = React.useState({ email: "", password: "" });
 
   const handleOnChange = ({ target }) => {
-    setLoginData({ [target.name]: target.value });
+    setLoginData({ ...loginData, [target.name]: target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await signInWithEmailAndPassword(
+        auth,
+        loginData.email,
+        loginData.password
+      );
+
+      setLoginData({ email: "", password: "" });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
@@ -16,7 +33,7 @@ const SignIn = () => {
       <h2>I already have an account.</h2>
       <span>Sign in with your email and password.</span>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <FormInput
           label="Email"
           onChange={handleOnChange}
